b602789aa496c4b113cae4311c78c800d2cdab12
WhiteW00lf <mocha_cookie_crumble@proton.me>
2025-07-13 16:11:51 +0000

    added streamlit and linechart,formatted.

--- kafka/consumer/consumers.py
+++ kafka/consumer/consumers.py
@@ -1,17 +1,58 @@
 from kafka import KafkaConsumer
-import sqlite3
+import streamlit as st
+import json
+import time
+import altair as alt
+import pandas as pd
 
-KAFKA_SERVER = '15.206.178.27:9092'
+KAFKA_SERVER = "15.206.178.27:9092"
 
 consumer = KafkaConsumer(
-    'cryptoprices',
+    "cryptoprices",
     bootstrap_servers=KAFKA_SERVER,
-    auto_offset_reset='earliest'
+    auto_offset_reset="earliest",
+    value_deserializer=lambda x: json.loads(x.decode("utf-8")),
 )
+
+# Start Streamlit app
+st.title("Bitcoin price tracker - near real-time")
+placeholder = st.empty()
+prices = []
 
 print("Consumer started, waiting for messages...")
 for message in consumer:
-    data = message.value.decode('utf-8')
-    print(f"Received message: {data}")
+    data = message.value
+    prices.append({"last_update": data["last_update"], "price": data["price"]})
 
+    df = pd.DataFrame(prices)
+    df["last_update"] = pd.to_datetime(df["last_update"])
+    df.sort_values(by="last_update", inplace=True)
 
+    chart = (
+        alt.Chart(df)
+        .mark_line(point=True)
+        .encode(
+            x="last_update:T",
+            y=alt.Y("price:Q", scale=alt.Scale(reverse=False)),
+        )
+        .properties(width=700, height=400)
+    )
+
+    text = (
+        alt.Chart(df)
+        .mark_text(
+            align="left",
+            dx=5,
+            dy=-5,  # adjust so text doesn't overlap point
+            fontSize=10,
+            color="black",
+        )
+        .encode(x="last_update:T", y="price:Q", text=alt.Text("price:Q", format=".2f"))
+    )
+
+    full_chart = chart + text
+
+    with placeholder.container():
+        st.altair_chart(full_chart, use_container_width=True)
+
+    time.sleep(5)  # To avoid overwhelming the Streamlit app with updates
--- kafka/producer/producers.py
+++ kafka/producer/producers.py
@@ -5,47 +5,35 @@ from dotenv import load_dotenv
 import os
 import time
 
-KAFKA_SERVER = '15.206.178.27:9092'
+KAFKA_SERVER = "15.206.178.27:9092"
 
-producer = KafkaProducer( bootstrap_servers=KAFKA_SERVER)
+producer = KafkaProducer(bootstrap_servers=KAFKA_SERVER)
 
-URL = 'https://api.freecryptoapi.com/v1/getData?symbol=BTC '
+URL = "https://api.freecryptoapi.com/v1/getData?symbol=BTC "
 
 load_dotenv()
 
 
+HEADERS = {"Authorization": f"Bearer {os.getenv('API_KEY')}"}
 
-HEADERS = {
-    'Authorization': f"Bearer {os.getenv('API_KEY')}"
-}
-
-
-    
 
 def make_request(url):
     data = requests.get(url, headers=HEADERS)
     default_dict = data.json()
-    crypto_name = default_dict['symbols'][0]['symbol']
-    price = default_dict['symbols'][0]['last']
-    last_update = default_dict['symbols'][0]['date']
-    data_in_json = json.dumps({
-        'crypto_name': crypto_name,
-        'price': price,
-        'last_update': last_update
-    })
+    crypto_name = default_dict["symbols"][0]["symbol"]
+    price = default_dict["symbols"][0]["last"]
+    last_update = default_dict["symbols"][0]["date"]
+    data_in_json = json.dumps(
+        {"crypto_name": crypto_name, "price": price, "last_update": last_update}
+    )
 
     print(f"Sending data to Kafka")
 
-    producer.send('cryptoprices', value=data_in_json.encode('utf-8'))
+    producer.send("cryptoprices", value=data_in_json.encode("utf-8"))
     producer.flush()
-
-
 
 
 while True:
 
     make_request(URL)
     time.sleep(5)
-
-
-
